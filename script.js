5a317a36c94be8cd40edc9b32b495bb351389f1e
biponroy47 <biponroy47@yahoo.com>
2022-09-02 03:05:03 -0400

    removed uneccessary comments

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".button");
 
-  //buttons
-  //0 = AC
-  //1 = display
-  //2 = 1
-  //3 = 2
-  //4 = 3
-  //5 = +
-  //6 = 4
-  //7 = 5
-  //8 = 6
-  //9 = -
-  //10 = 7
-  //11 = 8
-  //12 = 9
-  //13 = x
-  //14 = 0
-  //15 = .
-  //16 = =
-  //17 = ÷
-
   let sequence = "";
 
   const allClear = () => (sequence = "");
